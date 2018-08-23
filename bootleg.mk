5562ca789a25299f8061fc3e329dca5f6673a5bb
SonicBSV <sonicbsv@gmail.com>
2018-08-23 19:12:31 +0300

    X00TD: Rework for Bootleggers ROM

--- bootleg.mk
+++ bootleg.mk
@@ -18,24 +18,27 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bootleggers stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_BRAND := asus
+PRODUCT_BRAND := ASUS
 PRODUCT_DEVICE := X00TD
-PRODUCT_MANUFACTURER := asus
-PRODUCT_NAME := lineage_X00TD
-PRODUCT_MODEL := Zenfone Max Pro M1
+PRODUCT_MANUFACTURER := ASUS
+PRODUCT_NAME := bootleg_X00TD
+PRODUCT_MODEL := ZenFone Max Pro M1
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
-TARGET_VENDOR := asus
+TARGET_VENDOR := ASUS
 TARGET_VENDOR_PRODUCT_NAME := X00TD
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1309 release-keys"
-
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1534 release-keys" \
+    TARGET_DEVICE="X00TD" \
+    DEVICE_MAINTAINERS="SonicBSV"
+    
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:8.1.0/OPM1/15.2016.1805.318-20180712:user/release-keys
+BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:8.1.0/OPM1/15.2016.1808.323-20180813:user/release-keys
