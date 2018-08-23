ccd364a42caf17fd55fda19c122b182debbb21ac
SonicBSV <sonicbsv@gmail.com>
2018-08-24 01:15:21 +0300

    X00TD: revert D2TW (test)

--- rootdir/bin/init.qcom.sh
+++ rootdir/bin/init.qcom.sh
@@ -421,9 +421,6 @@ case "$target" in
         ;;
 esac
 
-# Set shared touchpanel nodes ownership (these are proc_symlinks to the real sysfs nodes)
-chown -LR system.system /proc/tpd_gesture
-
 #
 # Make modem config folder and copy firmware config to that folder for RIL
 #
