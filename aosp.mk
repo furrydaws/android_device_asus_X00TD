1c30d26d6d581c81d4bf98e11fc8c58e0f5b0ddc
Klajnor <Klajnor@gmail.com>
2018-09-26 16:55:41 +0300

    p

---
+++ aosp.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Bootleggers stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+
+# Inherit from X00TD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := ASUS
+PRODUCT_DEVICE := X00TD
+PRODUCT_MANUFACTURER := ASUS
+PRODUCT_NAME := bootleg_X00TD
+PRODUCT_MODEL := ZenFone Max Pro M1
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+TARGET_VENDOR := ASUS
+TARGET_VENDOR_PRODUCT_NAME := X00TD
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1534 release-keys" \
+    TARGET_DEVICE="X00TD" \
+    DEVICE_MAINTAINERS="Klajnor"
+    
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:8.1.0/OPM1/15.2016.1808.323-20180813:user/release-keys
