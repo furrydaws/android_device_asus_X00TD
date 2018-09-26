87595825629976ae1f5fa20b84918918e4bcbbc3
Klajnor <Klajnor@gmail.com>
2018-09-26 23:34:23 +0300

    -Snap Camera

--- device.mk
+++ device.mk
@@ -133,8 +133,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    libxml2 \
-    Snap
+    libxml2
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
